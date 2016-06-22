8f972ac0bb2ad287b1be0bb1f3268350d0c961d6
Bryan Berthou <bryan25.berthou@laposte.net>
2016-06-22 08:19:53 +0000

    refs#16
    PARTONS_release : initial import.

---
+++ src/main.cpp
@@ -0,0 +1,77 @@
+#include <partons/beans/List.h>
+#include <partons/Partons.h>
+#include <partons/services/automation/AutomationService.h>
+#include <partons/services/ObservableService.h>
+#include <partons/ServiceObjectRegistry.h>
+#include <partons/ServiceObjectTyped.h>
+#include <QtCore/qcoreapplication.h>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+std::vector<std::string> parseArguments(int argc, char** argv) {
+    if (argc <= 1) {
+        throw std::runtime_error(
+                "Missing argument, please provide one or more than one XML scenario file.");
+    }
+
+    std::vector<std::string> xmlScenarios;
+
+    for (unsigned int i = 1; i != argc; i++) {
+        xmlScenarios.push_back(argv[i]);
+    }
+
+    return xmlScenarios;
+}
+
+int main(int argc, char** argv) {
+
+    // Parse arguments to retrieve xml file path list.
+    std::vector<std::string> xmlScenarioFilePathList = parseArguments(argc,
+            argv);
+
+    QCoreApplication a(argc, argv);
+    Partons* pPartons = 0;
+
+    try {
+        // Init PARTONS application
+        pPartons = Partons::getInstance();
+        pPartons->init(argc, argv);
+
+        // Retrieve automation service parse scenario xml file and play it.
+        AutomationService* pAutomationService =
+                pPartons->getServiceObjectRegistry()->getAutomationService();
+
+        for (unsigned int i = 0; i != xmlScenarioFilePathList.size(); i++) {
+            Scenario* pScenario = pAutomationService->parseXMLFile(
+                    xmlScenarioFilePathList[i]);
+            pAutomationService->playScenario(pScenario);
+        }
+
+        ObservableService* pObservableService =
+                pPartons->getServiceObjectRegistry()->getObservableService();
+        std::cout << pObservableService->getResultList().toString()
+                << std::endl;
+
+        // Stop PARTONS application
+        if (pPartons) {
+            pPartons->close();
+            delete pPartons;
+            pPartons = 0;
+        }
+
+        // If there is an exception
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+
+        // Close PARTONS application propertly
+        if (pPartons) {
+            pPartons->close();
+            delete pPartons;
+            pPartons = 0;
+        }
+    }
+
+    return 0;
+}
